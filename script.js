c82497e71523d7ea287d71f8f8f42760fe4d782e
viniciustrembulak <vinicius_trembulak@outlook.com>
2024-02-26 23:11:55 -0300

    second

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-// variáveis
+import { Modal } from './modal.js'
 
+// variáveis
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height') 
+
 
 form.onsubmit = (event) => {
     event.preventDefault()
 
     const weight = inputWeight.value
     const height = inputHeight.value
-    imc = weight / height * 2
-    document.getElementsById('result').innerText = "imc"
     
-    
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+    Modal.open()
+}
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2)
 }
